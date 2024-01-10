24499063a8e77ace6c8ab55d0882c9cec51a3cd4
Jooyeon-Park <jooyeonpark0131@gmail.com>
2024-01-09 23:47:55 -0700

    done

---
+++ 554 Brick Wall.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        dic = {}  # 1:XX 2:XX ... Max nosplit = Min split
+        for row in wall:
+            sum = 0
+            for i in range(0, len(row) - 1):
+                sum += row[i]
+                if sum in dic.keys():
+                    dic[sum] += 1
+                else:
+                    dic[sum] = 1
+        
+        maxVal = 0
+        for i in dic.values():
+            maxVal = max(i,maxVal)
+        
+        return len(wall) - maxVal
