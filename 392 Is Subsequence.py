97528377bb5cbd2700035519d02a74056bd4cfab
Jooyeon-Park <jooyeonpark0131@gmail.com>
2024-01-23 00:35:36 -0700

    Done

--- 392 Is Subsequence.py
+++ 392 Is Subsequence.py
@@ -1,2 +1,16 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if s == "":
+            return True
+        i = 0
+        j = 0
+        while i < len(t):
+            if t[i] == s[j]:
+                j += 1
+            i += 1
+
+            if j >= len(s):
+                return True
+        return False
+
+                
