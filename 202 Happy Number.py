aa827289dc4ca9f06275313305ef8572bd9a3f42
Jooyeon-Park <jooyeonpark0131@gmail.com>
2024-01-29 00:02:34 -0700

    Done

--- 202 Happy Number.py
+++ 202 Happy Number.py
@@ -1,2 +1,20 @@
 class Solution:
+    def helper(self, n: int, visited: set) -> bool:
+        if n == 1:
+            return True
+        if n in visited:
+            return False
+        visited.add(n)
+        
+        new_n = 0
+        while n > 0:
+            digit = n % 10
+            new_n += digit * digit
+            n //= 10
+        
+        return self.helper(new_n, visited)
+
     def isHappy(self, n: int) -> bool:
+        visited = set()
+        return self.helper(n, visited)
+
