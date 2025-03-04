488254f461aa3c748b977f00df9333d5bcc653b6
Jooyeon-Park <jooyeonpark0131@gmail.com>
2025-03-04 00:00:03 -0700

    ing

---
+++ 238. Product of Array Except Self.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for i in range(0, len(nums)):
+            result.append(1)
+        prefix = 1
+        for i in range(0, len(nums)):
+            result[i] *= prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for j in range(len(nums)-1, 0, -1) 
